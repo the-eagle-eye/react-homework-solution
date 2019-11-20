c8b7995457729e0636c150733482b1e2ad690ecb
Elizabeth Manrique <ecmanrique+nopito@gmail.com>
2019-11-20 13:45:21 -0500

    Simplify components scafolding

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
-import App from "components/App";
+import App from "./App";
 
 function renderApp(AppComponent) {
   ReactDom.render(<AppComponent />, document.getElementById("root"));
