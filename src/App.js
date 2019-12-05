ffc04819c704cd5fe613e50f43d51bfa90c9b61b
Luigi <luigi@able.co>
2019-12-05 12:00:33 -0500

    add solution

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import PhotosList from "./components/PhotosList";
+import DeletedList from "./components/DeletedList";
+import axios from "axios";
 
-function ToDoList() {
+function App() {
+  const [photos, setPhotos] = useState([]);
+  const [removedPhotos, setRemovedPhotos] = useState([]);
+  const size = 20;
+
+  const removePhoto = photoSelected => {
+    const updatedPhotos = photos.filter(photo => photoSelected.id !== photo.id);
+    setPhotos(updatedPhotos);
+    setRemovedPhotos(removedPhotos.concat(photoSelected));
+  };
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/photos")
+      .then(response => {
+        setPhotos(response.data.slice(0, size));
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
   return (
-    <div>Hello</div>
+    <div>
+      <PhotosList photos={photos} removePhoto={removePhoto} heading="Photos" />
+      <DeletedList removedPhotos={removedPhotos} heading="Removed Photos" />
+    </div>
   );
 }
 
-export default ToDoList;
+export default App;
